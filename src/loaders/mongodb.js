1b909fdac51da4518e00c134752a312f00d35f60
akglbahadr <akgl.bahadr@gmail.com>
2021-12-25 01:26:15 +0300

    DB connection is made

---
+++ src/loaders/mongodb.js
@@ -0,0 +1,19 @@
+const Mongoose = require("mongoose");
+
+const db = Mongoose.connection;
+
+db.once("open", () => {
+  console.log("MongoDB'ye baglanti basarilidir..");
+});
+
+const connectDB = async () => {
+  const { DB_URI_STRING } = process.env;
+  await Mongoose.connect(`${ DB_URI_STRING }`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+module.exports = {
+  connectDB,
+};
