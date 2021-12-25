f34590c645305d668f60b3e394a807dae4809754
akglbahadr <akgl.bahadr@gmail.com>
2021-12-25 21:11:32 +0300

    validateSchema is defined

---
+++ src/validations/validate.js
@@ -0,0 +1,11 @@
+const Joi = require("joi");
+
+// create schema object
+const validateRecordReqSchema = Joi.object().keys({
+    startDate: Joi.date().iso().required(),
+    endDate: Joi.date().greater(Joi.ref("startDate")).iso().required(),
+    minCount: Joi.number().positive().integer().required(),
+    maxCount: Joi.number().positive().integer().required(),
+});
+
+module.exports = validateRecordReqSchema;
