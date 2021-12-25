eeeb5645b4e3496741da7a74db933183d2130f3d
akglbahadr <akgl.bahadr@gmail.com>
2021-12-25 21:12:37 +0300

    configuration are defined

--- src/config/server.js
+++ src/config/server.js
@@ -3,3 +3,16 @@ const dotenv = require("dotenv");
 module.exports = () => {
   dotenv.config();
 };
+
+
+
+/* const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+    port: process.env.PORT,
+    env: process.env.NODE_ENV,
+    mongodb_url : process.env.MONGODB_URL
+};
+ */
