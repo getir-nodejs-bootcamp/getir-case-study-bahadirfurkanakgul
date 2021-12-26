140705164d312c816c3aa2552f01ebd5082d5116
akglbahadr <akgl.bahadr@gmail.com>
2021-12-26 22:33:13 +0300

    Updated validation

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,25 +1,22 @@
-const BaseError = require('../utils/BaseError')
-const httpStatus = require('../utils/httpStatusCodes');
-const validateRecordReqSchema = require('../validations/validate')
+const ApiError = require("../errors/ApiError");
+const httpStatus = require("../utils/httpStatusCodes");
+const validateRecordReqSchema = require("../validations/validate");
 
 function validateRecordReq(req, res, next) {
-    validateRequest(req, next, validateRecordReqSchema);
+  validateRequest(req, next, validateRecordReqSchema);
 }
 
 function validateRequest(req, next, schema) {
-    const options = {
-        abortEarly: false, // include all errors
-        allowUnknown: true, // ignore unknown props
-        stripUnknown: true // remove unknown props
-    };
-    const { error, value } = schema.validate(req.body, options);
-    if (error) {
-        const errorMessage = error.details.map((details) => details.message).join(', ');
-        return next(new BaseError(httpStatus.BAD_REQUEST, errorMessage));
-
-    } else {
-        req.body = value;
-        next();
-    }
+  const options = { abortEarly: false }; // include all errors
+  const { error, value } = schema.validate(req.body, options);
+  if (error) {
+    const errorMessage = error.details
+      .map((details) => details.message)
+      .join(", ");
+    return next(new ApiError(errorMessage, httpStatus.BAD_REQUEST));
+  } else {
+    req.body = value;
+    next();
+  }
 }
 module.exports = { validateRecordReq };
--- src/validations/validate.js
+++ src/validations/validate.js
@@ -2,10 +2,14 @@ const Joi = require("joi");
 
 // create schema object
 const validateRecordReqSchema = Joi.object().keys({
-    startDate: Joi.date().iso().required(),
-    endDate: Joi.date().greater(Joi.ref("startDate")).iso().required(),
-    minCount: Joi.number().positive().integer().required(),
-    maxCount: Joi.number().positive().integer().required(),
+  startDate: Joi.date().iso().required(),
+  endDate: Joi.date().greater(Joi.ref("startDate")).iso().required(),
+  minCount: Joi.number().positive().integer().required(),
+  maxCount: Joi.number()
+    .positive()
+    .integer()
+    .required()
+    .greater(Joi.ref("minCount")),
 });
 
 module.exports = validateRecordReqSchema;
