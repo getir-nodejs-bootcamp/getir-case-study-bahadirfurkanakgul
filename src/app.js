f33ffdb6c9b76df58218f6478fb8f7c8923b8b0b
akglbahadr <akgl.bahadr@gmail.com>
2021-12-26 22:30:17 +0300

    app.js updated

--- src/app.js
+++ src/app.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const loaders = require('./loaders')
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const loaders = require("./loaders");
+const helmet = require("helmet");
+const cors = require("cors");
 const app = express();
-const returnError = require('./middlewares/errorHandler');
-const routes = require('./routes/recordRoute');
-const config = require('./config');
-const morgan = require('morgan'); // import morgan
+const ApiError = require("../src/errors/ApiError");
+const NotFoundError = require("../src/errors/NotFoundError");
+const routes = require("./routes/recordRoute");
+const config = require("./config");
+const morgan = require("morgan"); // import morgan
 const rfs = require("rotating-file-stream");
+const errorHandler = require("./middlewares/errorHandler");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./docs/swagger.json");
 
 config();
 loaders();
@@ -17,24 +20,26 @@ loaders();
 app.use(helmet());
 
 // enable morgan logger
-if(process.env.NODE_ENV === "production")
-    app.use(morgan(process.env.LOG_FORMAT || "dev"))
-else{
-    // create a log stream
-    const rfsStream = rfs.createStream(process.env.LOG_FILE || 'log.txt', {
-        size: process.env.LOG_SIZE || '10M',
-        interval: process.env.LOG_INTERVAL || '1d',
-        compress: 'gzip' // compress rotated files
-     });
+if (process.env.NODE_ENV === "production")
+  app.use(morgan(process.env.LOG_FORMAT || "dev"));
+else {
+  // create a log stream
+  const rfsStream = rfs.createStream(process.env.LOG_FILE || "log.txt", {
+    size: process.env.LOG_SIZE || "10M",
+    interval: process.env.LOG_INTERVAL || "1d",
+    compress: "gzip", // compress rotated files
+  });
 
-    app.use(morgan(process.env.LOG_FORMAT || "dev", {
-        stream: process.env.LOG_FILE ? rfsStream : process.stdout 
-    }));
+  app.use(
+    morgan(process.env.LOG_FORMAT || "dev", {
+      stream: process.env.LOG_FILE ? rfsStream : process.stdout,
+    })
+  );
 }
 
 // enable cors
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 // parse json request body
 app.use(express.json());
@@ -42,19 +47,22 @@ app.use(express.json());
 // parse urlencoded request body
 app.use(express.urlencoded({ extended: true }));
 
-// api routes
-app.use('/', routes);
-
-// handle error
-app.use(returnError);
+app.listen(process.env.APP_PORT || 3000, () => {
+  console.log(
+    `Application is running on port => ${process.env.APP_PORT || 3000}`
+  );
+  app.use("/records", routes);
+  app.use((req, res, next) => {
+    const error = new NotFoundError("Requested path does not exists.");
+    next(error);
+  });
+  app.use(errorHandler);
+});
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // if the Promise is rejected this will catch it
-process.on('unhandledRejection', error => {
-    throw error
-})
-
-app.listen(process.env.APP_PORT, () => {
-    console.log(`Application is running on ${process.env.APP_PORT}`);
-  });
+process.on("unhandledRejection", (error) => {
+  throw new ApiError();
+});
 
 module.exports = app;
