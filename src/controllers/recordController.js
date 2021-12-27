c8849a59dfcdbb7c66f90759224f2903edc1b10f
akglbahadr <akgl.bahadr@gmail.com>
2021-12-27 17:31:51 +0300

    unnecessary comments are removed

--- src/controllers/recordController.js
+++ src/controllers/recordController.js
@@ -4,19 +4,6 @@ const recordService = require("../services/recordService");
 const httpStatusCodes = require("../utils/httpStatusCodes");
 
 const getRecords = async (req, res, next) => {
-  // try {
-  //     const filter = {
-  //         'startDate' : req.body.startDate,
-  //         'endDate' : req.body.endDate,
-  //         'minCount' : req.body.minCount,
-  //         'maxCount' : req.body.maxCount
-  //     };
-
-  //     const result = await recordService.queryRecords(filter);
-  //     res.send(success({ records: [...result] }));
-  // } catch (error) {
-  //     next(error)
-  // }
   const filter = {
     startDate: req.body.startDate,
     endDate: req.body.endDate,
