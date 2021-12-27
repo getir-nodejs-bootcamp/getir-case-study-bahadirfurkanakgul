26f81f07cde4196cfdf582722437d5d9817c53fa
akglbahadr <akgl.bahadr@gmail.com>
2021-12-27 17:31:59 +0300

    integration tests are updated

--- src/tests/integration/integration.test.js
+++ src/tests/integration/integration.test.js
@@ -3,40 +3,50 @@ const app = require("../../app");
 const envPath = __dirname + "/../../.env";
 require("dotenv").config({ path: envPath });
 
-describe("POST /records", () => {
-  it("Successfull filtering of records.", async () => {
-    const body = {
-      startDate: "2016-01-26",
-      endDate: "2018-02-02",
-      minCount: 2700,
-      maxCount: 3000,
-    };
+
+test("If request body and request type is correct.", async () => {
+  const body = {
+    startDate: "2016-01-26",
+    endDate: "2018-02-02",
+    minCount: 2700,
+    maxCount: 3000,
+  };
     const resp = await request(app).post("/records").send(body);
-    expect(resp.headers["content-type"]).toEqual(
-      "application/json; charset=utf-8"
-    );
-    expect(resp.statusCode).toEqual(200);
-  });
-  it("Invalid request body", async () => {
-    const body = {
-      dummy: "dummy",
-    };
+      expect(resp.status).toEqual(200);
+      expect(resp.body).toHaveProperty("msg");
+      expect(resp.body).toHaveProperty("code");
+      expect(resp.body).toHaveProperty("records");
+});
+
+test("If start date is later than end date in request body.", async () => {
+  const body = {
+    startDate: "2018-01-26",
+    endDate: "2017-02-02",
+    minCount: 2700,
+    maxCount: 3000,
+  };
     const resp = await request(app).post("/records").send(body);
-    // .expect("Content-Type", /json/)
-    // .expect(400);
-  });
-  it("Min count is larger than max count request body.", async () => {
-    const body = {
-      startDate: "2016-01-26",
-      endDate: "2018-02-02",
-      minCount: 3200,
-      maxCount: 3000,
-    };
+      expect(resp.status).toEqual(400);
+});
+
+test("If min count is larger than max count in request body.", async () => {
+  const body = {
+    startDate: "2016-01-26",
+    endDate: "2018-02-02",
+    minCount: 2700,
+    maxCount: 1000,
+  };
     const resp = await request(app).post("/records").send(body);
-    // .expect("Content-Type", /json/)
-    // .expect(400);
-  });
-  it("Method other than POST request.", async () => {
-    const resp = await request(app).get("/records");
-  });
+      expect(resp.status).toEqual(400);
+});
+
+test("If there is a GET request.", async () => {
+  const body = {
+    startDate: "2016-01-26",
+    endDate: "2018-02-02",
+    minCount: 2700,
+    maxCount: 1000,
+  };
+    const resp = await request(app).get("/records").send(body);
+      expect(resp.status).toEqual(404);
 });
