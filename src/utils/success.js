2e36ae8ddb0d348422eb0f5ced67e8c5031c181e
akglbahadr <akgl.bahadr@gmail.com>
2021-12-27 17:32:20 +0300

    Final version

--- src/utils/success.js
+++ src/utils/success.js
@@ -4,22 +4,3 @@ module.exports = (data) => ({
     msg: 'Success',
     ...data,
 });
-
-
-/* const ERROR_RESPONSE = {
-	SUCCESS: {
-		"code": 0,
-		"msg": "Success"
-	},
-	CLIENT_ERROR: {
-		"code": 1
-	},
-	SERVER_ERROR: {
-		"code": 2,
-		"msg": "Something went wrong!"
-	},
-	NOT_FOUND: {
-		"code": 3,
-		"msg": "Not Found!"
-	},
-} */
