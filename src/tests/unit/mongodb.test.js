1393feae0e3ac4e1610e4d6e20805efdd2b38e69
akglbahadr <akgl.bahadr@gmail.com>
2021-12-27 21:01:02 +0300

    swagger is updated

--- src/tests/unit/mongodb.test.js
+++ src/tests/unit/mongodb.test.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const envPath = __dirname + "/../../../.env";
 require("dotenv").config({ path: envPath });
 
-test("Successful connection to database", () => {
-    // process.env.DB_URI_STRING =
-    //   "mongodb+srv://challengeUser:WUMglwNBaydH8Yvu@challenge-xzwqd.mongodb.net/getir-case-study?retryWrites=true";
-    connectDB();
+test("Successful connection to database", async () => {
+    await connectDB();
 });
 
 
