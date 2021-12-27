4a27fe849c937594ad6146a930183c162e78e54e
akglbahadr <akgl.bahadr@gmail.com>
2021-12-27 21:32:59 +0300

    Removed unnecessary requirements

--- src/tests/integration/integration.test.js
+++ src/tests/integration/integration.test.js
@@ -1,8 +1,5 @@
 const request = require("supertest");
 const app = require("../../app");
-const envPath = __dirname + "/../../.env";
-require("dotenv").config({ path: envPath });
-
 
 test("If request body and request type is correct.", async () => {
   const body = {
@@ -18,7 +15,7 @@ test("If request body and request type is correct.", async () => {
       expect(resp.body).toHaveProperty("records");
 });
 
-test("If start date is later than end date in request body.", async () => {
+test("If startDate is later than endDate in request body.", async () => {
   const body = {
     startDate: "2018-01-26",
     endDate: "2017-02-02",
@@ -29,7 +26,7 @@ test("If start date is later than end date in request body.", async () => {
       expect(resp.status).toEqual(400);
 });
 
-test("If min count is larger than max count in request body.", async () => {
+test("If minCount is larger than maxCount in request body.", async () => {
   const body = {
     startDate: "2016-01-26",
     endDate: "2018-02-02",
