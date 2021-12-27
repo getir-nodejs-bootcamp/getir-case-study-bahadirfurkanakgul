36935095aa935aa817bef5c490113ab72dca34e1
akglbahadr <akgl.bahadr@gmail.com>
2021-12-27 17:32:35 +0300

    Code structure is fixed

--- src/validations/validate.js
+++ src/validations/validate.js
@@ -5,11 +5,7 @@ const validateRecordReqSchema = Joi.object().keys({
   startDate: Joi.date().iso().required(),
   endDate: Joi.date().greater(Joi.ref("startDate")).iso().required(),
   minCount: Joi.number().positive().integer().required(),
-  maxCount: Joi.number()
-    .positive()
-    .integer()
-    .required()
-    .greater(Joi.ref("minCount")),
+  maxCount: Joi.number().positive().integer().required().greater(Joi.ref("minCount")),
 });
 
 module.exports = validateRecordReqSchema;
